0d640c1ecc16a4e565a9ee880d7dc250b2481093
trishamitch <93698247+trishamitch@users.noreply.github.com>
2024-02-26 19:54:28 -0800

    Update home.php

--- with login user and admin/home.php
+++ with login user and admin/home.php
@@ -8,11 +8,13 @@ if (isset($_SESSION['id']) && isset($_SESSION['uname'])) {
 <html>
 <head>
 	<title>HOME</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="home_style.css">
 </head>
 <body>
      <h1>Hello, <?php echo $_SESSION['fname']; ?></h1>
-     <a href="index.php">Logout</a>
+     <form action="index.php" method="POST">
+        <button type="submit" name="logout">Logout</button>
+    </form>
 </body>
 </html>
 
