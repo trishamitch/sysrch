3904d445491bcd2e0d8e569dfcc7d610dc54dcb3
trishamitch <93698247+trishamitch@users.noreply.github.com>
2024-02-29 12:55:24 +0800

    Update home.php

--- with login user and admin/home.php
+++ with login user and admin/home.php
@@ -1,69 +1,68 @@
-<?php 
+<?php
 session_start();
 
 if (isset($_SESSION['id']) && isset($_SESSION['uname'])) {
 
- ?>
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>HOME</title>
-	<link rel="stylesheet" type="text/css" href="home_style.css">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>HOME</title>
+    <link rel="stylesheet" type="text/css" href="home_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-     <h1>Welcome <?php echo ucfirst($_SESSION['fname']); ?></h1>
-     <form action="index.php" method="POST">
-        <button type="submit" name="logout"><i class="fa fa-sign-out"></i>Logout</button>
+    <h1>Welcome, <?php echo ucfirst($_SESSION['fname']); ?></h1>
+
+    <form action="index.php" method="POST"> <button type="submit" name="logout"><i class="fa fa-sign-out"></i>Logout</button>
     </form>
+
     <div class="ag-format-container">
-          <div class="ag-courses_box">
-               <div class="ag-courses_item">
-                    <a href="#" class="ag-courses-item_link">
-                    <div class="ag-courses-item_bg"></div>
+        <div class="ag-courses_box">
+            <div class="ag-courses_item">
+                <a href="edit_profile.php" class="ag-courses-item_link"> <div class="ag-courses-item_bg"></div>
 
                     <div class="ag-courses-item_title">
-                         Edit Profile
+                        Edit Profile
                     </div>
-                    </a>
-               </div>
+                </a>
+            </div>
 
-          <div class="ag-courses_item">
-               <a href="#" class="ag-courses-item_link">
-               <div class="ag-courses-item_bg"></div>
+            <div class="ag-courses_item">
+                <a href="sit_in.php" class="ag-courses-item_link"> <div class="ag-courses-item_bg"></div>
 
-               <div class="ag-courses-item_title">
-                   Sit-in
-               </div>
-               </a>
-          </div>
+                    <div class="ag-courses-item_title">
+                        Sit-in
+                    </div>
+                </a>
+            </div>
 
-          <div class="ag-courses_item">
-               <a href="#" class="ag-courses-item_link">
-               <div class="ag-courses-item_bg"></div>
+            <div class="ag-courses_item">
+                <a href="session.php" class="ag-courses-item_link"> <div class="ag-courses-item_bg"></div>
 
-               <div class="ag-courses-item_title">
-                    View Remaining Sessions
-               </div>
-               </a>
-          </div>
+                    <div class="ag-courses-item_title">
+                        Remaining Session
+                    </div>
+                </a>
+            </div>
 
-          <div class="ag-courses_item">
-               <a href="#" class="ag-courses-item_link">
-               <div class="ag-courses-item_bg"></div>
+            <div class="ag-courses_item">
+                <a href="session.php" class="ag-courses-item_link"> <div class="ag-courses-item_bg"></div>
 
-               <div class="ag-courses-item_title">
-                    Make Reservation
-               </div>
-               </a>
-          </div>
-     </div>     
+                    <div class="ag-courses-item_title">
+                        Reservation
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
 
-<?php 
-}else{
-     header("Location: index.php");
-     exit();
+<?php
+} else {
+    header("Location: index.php");
+    exit();
 }
- ?>
+?>
